cb727afc51aa566928579e417eba69d912d2c708
Isaiah Harrison <ijh5005@outlook.com>
2019-09-23 21:34:04 -0400

    made responsive

--- src/Home/About/index.js
+++ src/Home/About/index.js
@@ -7,7 +7,7 @@ function About() {
   return (<div id="about" className="flex">
     <div id="about_leftPanel" className="flex">
       <div id="home_Body">
-        <p>Blakio LLC offers our customers a custom software solution for their needs.</p>
+        <p>Blakio LLC is a black-owned tech company offering customers a custom software solution for their needs.</p>
         <p>The future is digital! We are here to make your company digital as well by evolving your software or current system so you can focus on bringing your ideas to life. Our knowledge and experience translate to added value and peace of mind for our customers.</p>
       </div>
     </div>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './css/index.css';
 import './css/animations.css';
 import './css/home.css';
 import './css/development.css';
+import './css/mediaquery.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
