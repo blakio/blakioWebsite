8f3f592fce13200a59331ca6af17227fa6cc9768
Isaiah Harrison <ijh5005@outlook.com>
2019-09-24 20:14:57 -0400

    adjustments

--- src/Home/About/index.js
+++ src/Home/About/index.js
@@ -7,7 +7,10 @@ function About() {
   return (<div id="about" className="flex">
     <div id="about_leftPanel" className="flex">
       <div id="home_Body">
-        <p style={{fontWeight: 300}}>Blakio LLC is a black-owned tech company offering customers a custom software solution for their needs.</p>
+        <p style={{
+          fontWeight: 900,
+          fontSize: "2.4em"
+        }}>Blakio LLC is a black-owned tech company offering customers a custom software solution for their needs.</p>
         <p>The future is digital! We are here to make your company digital as well by evolving your system so you can focus on your business.</p>
         <p>Our knowledge and experience translate to added value and peace of mind for our customers.</p>
       </div>
