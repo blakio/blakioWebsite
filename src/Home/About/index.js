da2c1e7f47f5ae960332f68b0e0da844bc16553b
Isaiah Harrison <ijh5005@outlook.com>
2019-10-12 12:02:15 -0400

    ui changes

--- src/Home/About/index.js
+++ src/Home/About/index.js
@@ -5,14 +5,16 @@ import React from 'react';
 function About() {
 
   return (<div id="about" className="flex">
-    <div id="about_leftPanel" className="flex">
-      <div id="home_Body">
+    <div id="about_leftPanel" className="about flex">
+      <div id="home_Body" className="about">
         <p style={{
           fontWeight: 900,
-          fontSize: "2.4em"
-        }}>Blakio LLC is a black-owned tech company offering customers a custom software solution for their needs.</p>
-        <p>The future is digital! We are here to make your company digital as well by evolving your system so you can focus on your business.</p>
-        <p>Our knowledge and experience translate to added value and peace of mind for our customers.</p>
+          fontSize: "3.6em",
+          width: "80%"
+        }}>Blakio LLC Is A Black-Owned Tech Company<span className="icolor">...</span></p>
+        <p><span className="icolor">...</span>Offering customers a custom software solution for their needs<span className="icolor">.</span></p>
+        <p>The future is digital<span className="icolor">.</span> We are here to make your company digital as well by evolving your system so you can focus on your business<span className="icolor">.</span></p>
+        <p>Our knowledge and experience translate to added value and peace of mind for our customers<span className="icolor">.</span></p>
       </div>
     </div>
     <div id="about_rightPanel"></div>
