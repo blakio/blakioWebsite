65a68f6ec49dbb1df5f8fd5855ad727b93f82814
Isaiah Harrison <ijh5005@outlook.com>
2020-02-05 10:13:48 -0500

    pricing css

--- src/Home/Pricing/index.js
+++ src/Home/Pricing/index.js
@@ -8,10 +8,10 @@ const Pricing = () => {
   return (<div id="pricingSection" className="flex">
     <p className="title" >PRICING</p>
 
-    <p className="pricingDescription">Dashboard Subscription <span className="big">$4000</span> / yr</p>
+    <p className="pricingDescription"><span className="green big">$4000</span> / yr</p>
 
     <p className="title">HOW IT WORKS</p>
-    <p className="pricingDescription">Add your first dashboard feature for a one time charge from <span className="big">$0</span></p>
+    <p className="pricingDescription">Add your first dashboard feature for a one time charge from <span className="green big">$0</span></p>
     <br />
     <br />
     <p className="pricingDescription">Need more features in the future? No problem.</p>
