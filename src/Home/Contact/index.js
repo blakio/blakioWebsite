f12d5ee4197cfa54648bb905a37dc9bc209d94be
blakio <54945729+blakio@users.noreply.github.com>
2020-08-09 18:31:18 -0400

    Update index.js

--- src/Home/Contact/index.js
+++ src/Home/Contact/index.js
@@ -54,17 +54,19 @@ const Contact = () => {
     <div className="form-style-2">
       <form action="" method="post">
         <label for="field1">
-          <span>Name</span>
-          <input type="text" className="input-field" name="field1" value={name} onChange={nameChange.bind(this)}/>
+          <span>Call or text: 814 753 0157</span>
+          <span>Email: ijh5005@outlook.com</span>
+          {/*<span>Name</span>*/}
+          {/*<input type="text" className="input-field" name="field1" value={name} onChange={nameChange.bind(this)}/>*/}
         </label>
-        <label for="field2">
+       {/*<label for="field2">
           <span>Email</span>
           <input type="text" className="input-field" name="field2" value={contact} onChange={contactChange.bind(this)}/>
         </label>
         <label for="field5"><span>Message</span>
           <textarea name="field5" className="textarea-field" value={message} onChange={messageChange.bind(this)}></textarea></label>
         <label>
-        <span> </span><input type="submit" value="Submit" onClick={submitEmail.bind(this)} /></label>
+        <span> </span><input type="submit" value="Submit" onClick={submitEmail.bind(this)} /></label>*/}
       </form>
     </div>
   </div>)
